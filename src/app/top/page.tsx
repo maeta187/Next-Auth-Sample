3ee1aadf38a9736ffbb16a6694d677d7c41a500b
maeta187 <nexus18789@gmail.com>
2023-07-22 13:54:02 +0900

    ✨ : getServerSessionの実装

--- src/app/top/page.tsx
+++ src/app/top/page.tsx
@@ -4,18 +4,29 @@ import {
   ProfileButton,
   RegisterButton
 } from '@/components/buttons'
+import { authOptions } from '@/lib/auth'
+import { getServerSession } from 'next-auth'
+import { User } from './user'
 
-export default function Page() {
+export default async function Page() {
+  const session = await getServerSession(authOptions)
+  console.log(session)
+
   return (
-    <main className='flex justify-center items-center h-screen'>
+    <main className='flex flex-col justify-center items-center text-center h-screen'>
       <div>
         <LoginButton />
         <LogoutButton />
-      </div>
-      <div>
         <RegisterButton />
         <ProfileButton />
       </div>
+      <div className='mt-5'>
+        <h1>Server Session</h1>
+        <pre>
+          <code>{JSON.stringify(session)}</code>
+        </pre>
+      </div>
+      <User />
     </main>
   )
 }
