cdba0a06054b55756d4a23b8c1370d93760ffbf0
maeta187 <nexus18789@gmail.com>
2023-07-23 15:26:42 +0900

    👔 : APIでルート保護の実装

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -5,6 +5,14 @@ import { NextResponse } from 'next/server'
 export async function GET(request: Request) {
   const session = await getServerSession(authOptions)
 
+  /** APIでルート保護する場合 */
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ status: 'fail', message: 'You are not logged in' }),
+      { status: 401 }
+    )
+  }
+
   return NextResponse.json({
     authenticated: !!session,
     session
